a1a773fd7ca174bcfc0433428a017b29223cd66a
XeyPlays <78436425+XeyPlays@users.noreply.github.com>
2021-02-02 19:36:53 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // Format: https://api.github.com/repos/{owner}/{repo}/issues/
 const GITHUB_ISSUES_LINK =
-  "https://github.com/XeyPlays/link-db/issues";
+  "https://api.github.com/repos/XeyPlays/link-db/issues";
 
 const PATH_SEGMENTS_TO_SKIP = 0;
 
