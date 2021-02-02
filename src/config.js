c769243a702f59b3e1073f5936a3344a69226fba
XeyPlays <78436425+XeyPlays@users.noreply.github.com>
2021-02-02 19:00:41 +0100

    Delete config.js

--- src/config.js
+++
@@ -1,7 +0,0 @@
-// Format: https://api.github.com/repos/{owner}/{repo}/issues/
-const GITHUB_ISSUES_LINK =
-  "https://github.com/XeyPlays/link-db/issues";
-
-const PATH_SEGMENTS_TO_SKIP = 0;
-
-const HOST = "dsmp.ml";
